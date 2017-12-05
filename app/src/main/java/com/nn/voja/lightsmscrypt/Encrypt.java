9305e8fb10598e7f37b6cd9604ab71d77c0031db
Vojislav Ristivojevic <batica@gmail.com>
2017-12-05 15:59:05 +0100

    Added activities

---
+++ app/src/main/java/com/nn/voja/lightsmscrypt/Encrypt.java
@@ -0,0 +1,13 @@
+package com.nn.voja.lightsmscrypt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Encrypt extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypt);
+    }
+}
